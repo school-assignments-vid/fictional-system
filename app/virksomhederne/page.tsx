977d4332f3dce4330aa5d7e6bc71b16f80582b42
Sobbing Cat <kenshin@kukiku.org>
2025-09-05 09:35:52 +0200

    Update

--- app/virksomhederne/page.tsx
+++ app/virksomhederne/page.tsx
@@ -1,9 +1,44 @@
+import Link from "next/link";
+
 export default function Page() {
   return (
     <>
-      <div>
-        <h1>Content goes into this thing</h1>
-      </div>
+      <section className="relative bg-[url(/images/forside/paper_bg.webp)] bg-cover">
+        <div className="bg-primary/80 w-full min-h-120 flex justify-center items-center flex-col gap-8 font-headline text-center p-4">
+          <h1 className="text-4xl md:text-6xl font-bold text-shadow-lg text-white">
+            Brancheområderne
+          </h1>
+          <div className="text-white flex flex-col gap-6 font-headline text-xl px-4">
+            <Link
+              href={"virksomhederne/foedevarer-jordbrug-og-oplevelser"}
+              className="border-white font-semibold border-2 py-2 px-4 rounded-sm"
+            >
+              Fødevarer, jordbrug og oplevelser
+            </Link>
+            <Link
+              href={"virksomhederne/foedevarer-jordbrug-og-oplevelser"}
+              className="border-white font-semibold border-2 py-2 px-4 rounded-sm"
+            >
+              Kontor, handel og forretningsservice
+            </Link>
+            <Link
+              href={"virksomhederne/foedevarer-jordbrug-og-oplevelser"}
+              className="border-white font-semibold border-2 py-2 px-4 rounded-sm"
+            >
+              Teknologi, byggeri og transport
+            </Link>
+            <Link
+              href={"virksomhederne/foedevarer-jordbrug-og-oplevelser"}
+              className="border-white font-semibold border-2 py-2 px-4 rounded-sm"
+            >
+              Omsorg, sundhed og pædagogik
+            </Link>
+          </div>
+        </div>
+      </section>
+      <section className="container mx-auto">
+        
+      </section>
     </>
   );
 }
